4263add4952a19f49092f1b5e733852cacd233c9
Drini Cami <cdrini@gmail.com>
2024-08-31 08:18:32 +0200

    Add crunch step to OCR printer to avoid offset errors

--- tocky/ocr/printer.py
+++ tocky/ocr/printer.py
@@ -1,5 +1,5 @@
 import math
-from typing import Literal
+from typing import Literal, cast
 from lxml import etree
 from rtree import index
 
@@ -54,42 +54,55 @@ def ocr_printer_canvas(djvu_page: str | etree._Element) -> str:
     canvas_height = math.ceil(img_height * conv_factor)
     canvas = ((" " * canvas_width + '\n') * canvas_height)[:-1]
 
-    used_regions = index.Index()
-    rect_index = 0
-    for pagecol in root.findall('.//PAGECOLUMN'):
-        for par in pagecol.findall('.//PARAGRAPH'):
-            for line in par.findall('.//LINE'):
-                # log('LINE')
-                line_row = None
-                for word in line.findall('.//WORD'):
-                    coords = word.xpath('./@coords')[0].split(',')
-                    left = int(coords[0])
-                    bottom = int(coords[1])
-                    right = int(coords[2])
-                    top = int(coords[3])
+    def build_index(conv_factor: float, line_rounding_size: int) -> index.Index:
+        regions = index.Index()
+        rect_index = 0
+        for pagecol in root.findall('.//PAGECOLUMN'):
+            for par in pagecol.findall('.//PARAGRAPH'):
+                for line in par.findall('.//LINE'):
+                    # log('LINE')
+                    line_row = None
+                    for word in line.findall('.//WORD'):
+                        coords = word.xpath('./@coords')[0].split(',')
+                        left = int(coords[0])
+                        bottom = int(coords[1])
+                        right = int(coords[2])
+                        top = int(coords[3])
 
-                    baseline = round((top + (bottom - top) / 3) / line_rounding_size) * line_rounding_size
-                    line_row = math.floor(baseline * conv_factor) if line_row is None else line_row
-                    col = math.floor(left * conv_factor)
+                        baseline = round((top + (bottom - top) / 3) / line_rounding_size) * line_rounding_size
+                        line_row = math.floor(baseline * conv_factor) if line_row is None else line_row
+                        col = math.floor(left * conv_factor)
 
-                    while list(used_regions.intersection(rect := (col, line_row, col + len(word.text), line_row + 1))):
-                      col += 1
+                        while list(regions.intersection(rect := (col, line_row, col + len(word.text), line_row + 1))):
+                            col += 1
 
-                    used_regions.insert(rect_index, rect)
-                    rect_index += 1
+                        regions.insert(rect_index, rect, obj=word.text)
+                        rect_index += 1
 
-                    start_idx = (canvas_width + 1) * line_row + col
-                    end_idx = start_idx + len(word.text)
-                    shift = 0
-                    max_shift = 5
-                    # log(f'{line_row}:{col}\t{coords}\t{word.text}')
-                    # while not canvas[max(0, start_idx - 1):(end_idx + 1)].isspace():
-                    #   start_idx += 1
-                    #   end_idx += 1
-                    #   shift += 1
-                    #   if shift >= max_shift:
-                    #     break
-                    canvas = canvas[0:start_idx] + word.text + canvas[end_idx:]
+        return regions
+
+
+    def build_canvas(regions: index.Index, canvas_width: int, canvas_height: int) -> str:
+        canvas = ((" " * canvas_width + '\n') * canvas_height)[:-1]
+        for item in regions.intersection(regions.bounds, objects=True):
+            word = cast(str, item.object)
+            line_row = int(item.bbox[1])
+            col = int(item.bbox[0])
+
+            start_idx = (canvas_width + 1) * line_row + col
+            end_idx = start_idx + len(word)
+            # shift = 0
+            # max_shift = 5
+            # log(f'{line_row}:{col}\t{coords}\t{word.text}')
+            # while not canvas[max(0, start_idx - 1):(end_idx + 1)].isspace():
+            #   start_idx += 1
+            #   end_idx += 1
+            #   shift += 1
+            #   if shift >= max_shift:
+            #     break
+            canvas = canvas[0:start_idx] + word + canvas[end_idx:]
+        return canvas
+       
 
     def crop_text_canvas(canvas: str, used_regions: index.Index) -> str:
       lines = canvas.split('\n')
@@ -114,7 +127,45 @@ def ocr_printer_canvas(djvu_page: str | etree._Element) -> str:
         return re.sub(r' ' * (min_newline_chain -1) + '( *)', r'\1', s, flags=re.MULTILINE)
         # return re.sub(r'(\S) +', r'\1 ', s)
 
-    return collapse_newlines(crop_text_canvas(canvas, used_regions))
+    def crunch_solos(regions: index.Index):
+        def shrink_rect(rect: tuple[float, float, float, float]) -> tuple[float, float, float, float]:
+            return (rect[0] + 0.1, rect[1] + 0.1, rect[2] - 0.1, rect[3] - 0.1)
+
+        prev_rects_on_line = []
+        for row_i in range(int(regions.bounds[1]), int(regions.bounds[3] + 1)):
+            row_rect = (0, row_i + 0.1, canvas_width, row_i + 0.9)
+            rects_on_line = list(n for n in regions.intersection(row_rect, objects=True))
+
+            # Can I move up?
+            if len(rects_on_line) == 1 and prev_rects_on_line:
+                item = rects_on_line[0]
+                rect = item.bbox
+                new_rect = (rect[0], rect[1] - 1, rect[2], rect[3] - 1)
+                
+                if not any(regions.intersection(shrink_rect(new_rect))):
+                    regions.delete(item.id, rect)
+                    regions.insert(item.id, new_rect, obj=item.object)
+
+                    rects_on_line = list(n for n in regions.intersection(row_rect, objects=True))
+            # Can I pull predecessor?
+            elif len(prev_rects_on_line) == 1 and rects_on_line:
+                item = prev_rects_on_line[0]
+                rect = item.bbox
+                new_rect = (rect[0], rect[1] + 1, rect[2], rect[3] + 1)
+                if not any(regions.intersection(shrink_rect(new_rect))):
+                    regions.delete(item.id, rect)
+                    regions.insert(item.id, new_rect, obj=item.object)
+
+                    rects_on_line = list(n for n in regions.intersection(row_rect, objects=True))
+
+            prev_rects_on_line = rects_on_line
+
+
+    regions = build_index(conv_factor, line_rounding_size)
+    crunch_solos(regions)
+    canvas = build_canvas(regions, canvas_width, canvas_height)
+
+    return collapse_newlines(crop_text_canvas(canvas, regions))
 
 def print_ocr(djvu_xml: str | etree._Element, printer: Literal['canvas', 'linear']='canvas') -> str:
   if printer == 'canvas':
